42c38f483865f497a1281a6c09ccb0565067b88a
Haiber H. Galindo <h.galindos@uniandes.edu.co>
2023-01-09 11:22:07 -0500

    Initial project-test Get museums without private var

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ function routerApi(app) {
   //Agregamos a la app el pattern path del servicio
   app.use('/aws/cloudapp/v1', router);
   //Agregamos los paths de los servicios de museos
-  router.use('/museums', museumRouter);
+  // router.use('/museums', museumRouter);
   //Agregamos los paths de los servicios de museos
   router.use('/security', securityRouter);
   //Si queremos definir varias enpoint con diferentes versiones se haria de la siguiente forma
