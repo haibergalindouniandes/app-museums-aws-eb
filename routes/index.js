49760e577610db8ec9fa25c0c2b52f852f1117db
Haiber H. Galindo <h.galindos@uniandes.edu.co>
2023-01-09 12:22:32 -0500

    Initial project-test memory storage

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// const securityRouter = require('./security.router');
+const securityRouter = require('./security.router');
 const museumRouter = require('./museum.router');
 
 //Funcion que tiene todos los enrutamientos de la aplicación
@@ -9,9 +9,9 @@ function routerApi(app) {
   //Agregamos a la app el pattern path del servicio
   app.use('/aws/cloudapp/v1', router);
   //Agregamos los paths de los servicios de museos
-  router.use('/museums', museumRouter);
+  router.use('/security', securityRouter);
   //Agregamos los paths de los servicios de museos
-  // router.use('/security', securityRouter);
+  router.use('/museums', museumRouter);
   //Si queremos definir varias enpoint con diferentes versiones se haria de la siguiente forma
   // const routerV2 = express.Router();
   // app.use('/aws/cloudapp/v2', routerV2);
