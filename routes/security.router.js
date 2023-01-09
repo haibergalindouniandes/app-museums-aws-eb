84043762d72edb83272765763df0d75e35e36d75
Haiber H. Galindo <h.galindos@uniandes.edu.co>
2023-01-09 11:27:12 -0500

    Initial project-test Get museums without private var

--- routes/security.router.js
+++ routes/security.router.js
@@ -24,4 +24,31 @@ router.post('/generate',
     }
 );
 
+//Borrar
+
+router.get('/',
+    // verifyToken,
+    async (req, res, next) => {
+        // Forma convencional para obtener el path param a buscar
+        // const size = req.query.size;
+        // Destructiracion de objetos ecmascript para obtener solo los elementos que necesito
+        try {
+            res.status(200).json({
+                statusCode: 200,
+                message: 'Success transaction',
+                data: [{
+                    name: "Museum Created By API",
+                    description: "enim in tempor turpis nec euismod scelerisque quam turpis adipiscing lorem",
+                    address: "9212 Packers Hill",
+                    city: "Bogota",
+                    image: "https://secretldn.com/wp-content/uploads/2022/01/summer-of-culture-british-museum-1024x606-1-666x394.jpg",
+                    isBlocked: false,
+                    id: 1
+                }]
+            });
+        } catch (error) {
+            next(error);
+        }
+    });
+
 module.exports = router;
