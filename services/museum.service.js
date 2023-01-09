3a3be93f29254a94caf3ead8f7a2e1317aa48151
Haiber H. Galindo <h.galindos@uniandes.edu.co>
2023-01-09 11:13:33 -0500

    Initial project-test Get museums without private var

--- services/museum.service.js
+++ services/museum.service.js
@@ -3,22 +3,15 @@ class MuseumServices {
   constructor() { }
 
   async find(limit, type) {
-    var listMuseums = [];
-    for (let index = 0; index < limit; index++) {
-      var museum = {
-        name: `Museum Created By API - ${index}`,
-        description: `enim in tempor turpis nec euismod scelerisque quam turpis adipiscing lorem - ${index}`,
-        address: `9212 Packers Hill - ${index}`,
-        city: `Bogota${index}`,
-        image: "https://secretldn.com/wp-content/uploads/2022/01/summer-of-culture-british-museum-1024x606-1-666x394.jpg",
-        isBlocked: false,
-        id: index
-      };
-
-      listMuseums.push(museum);
-    }
-
-    return listMuseums;
+    return [{
+      name: `Museum Created By API`,
+      description: `enim in tempor turpis nec euismod scelerisque quam turpis adipiscing lorem`,
+      address: `9212 Packers Hill`,
+      city: `Bogota`,
+      image: "https://secretldn.com/wp-content/uploads/2022/01/summer-of-culture-british-museum-1024x606-1-666x394.jpg",
+      isBlocked: false,
+      id: 1
+    }];
   }
 
 }
